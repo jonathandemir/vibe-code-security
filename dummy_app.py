302399490967bd36a79de391ace45750c0896384
Jonathan <jonathandemir@icloud.com>
2026-02-21 00:52:30 +0100

    semgreb

--- dummy_app.py
+++ dummy_app.py
@@ -1,5 +1,11 @@
-# dummy_app.py
-def connect():
+import sqlite3
 
-
-    print("Verbinde... hopefully this works")
+def get_user_data(user_id):
+    # EXTREM UNSICHER: Direkte String-Verkettung in SQL!
+    query = "SELECT * FROM users WHERE id = " + user_id
+    
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    
+    return cursor.fetchall()
